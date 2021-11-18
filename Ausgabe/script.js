e372b1c829a1e1adf7a24ebd27e153d70de097ba
Vincent Jones <vinc.alex.jones@gmail.com>
2021-11-18 11:18:33 +0100

    ben drowned

--- Ausgabe/script.js
+++ Ausgabe/script.js
@@ -1,11 +1,12 @@
-for (let i = 0; i < 4 ; i++){
-  for (let b = 0; b < 4; b++) {
-    
-    document.write('x')
-    document.write('\n')
-    document.writeln('o')
-    
-  }
-  
-
+for (let i = 0; i < 2 ; i++){
+ for (let x = 0; x < 4; x++) {
+    document.write("x");
+ }
+ document.write("<br>");
+ for (let o = 0; o < 4; o++) {
+    document.write("o");
+ }
+ document.write("<br>");
 }
+
+
