836b8ace85ac233e9c49f5ff57e660552b84d463
Vincent Jones <vinc.alex.jones@gmail.com>
2021-11-23 16:24:03 +0100

    Aufgabe 09 Fertig

--- Ausgabe/script.js
+++ Ausgabe/script.js
@@ -9,4 +9,32 @@ for (let i = 0; i < 2 ; i++){
  document.write("<br>");
 }
 
+document.write("<br>");
 
+document.write("<br>");
+
+document.write("<br>");
+
+let summe = 0;
+
+for (let i = 0; i < 100; i++) {
+   summe = summe + i;
+   i = i + 1;   
+}
+
+console.log(summe)
+
+for (let b = 0; b < 3; b++) {
+   for (let l = 0; l < 1; l++) {
+      document.write("x")
+      }
+
+   document.write("<br>")
+
+   
+      
+   }
+
+   for (let k = 0; k < 4; k++) {
+      document.write("x")
+}
