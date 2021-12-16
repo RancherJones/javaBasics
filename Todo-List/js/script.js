a743873eaa462b6502b2df256f31bad38cd951f9
Vincent Jones <vinc.alex.jones@gmail.com>
2021-12-16 10:16:57 +0100

    Fehler Tasks Corrigiert

--- Todo-List/js/script.js
+++ Todo-List/js/script.js
@@ -2,33 +2,33 @@
 
 
 
-const task = [];
-printTaskList();
+const tasks = [];
+console.log(tasks);
 
 
 
-document.getElementById("addTask").addEventListener("click",function(){
+document.getElementById("addTask").addEventListener("click", function () {
   addTask();
 });
 
-function addTask(){
+function addTask() {
   let taskName = document.getElementById("textNewTask").value;
   let taskResponsible = document.getElementById("textResponsible").value;
-  let task = {name:taskName, responsible:taskResponsible, isDone:false}
-  task.push(task);
+  let task = { name: taskName, responsible: taskResponsible, isDone: false }
+  tasks.push(task);
   printTaskList();
 }
 
-function printTaskList(){
-document.getElementById("taskList").innerHTML = getHtmlTasks();
+function printTaskList() {
+  document.getElementById("taskList").innerHTML = getHtmlTasks();
 
 }
 
-function getHtmlTasks(){
+function getHtmlTasks() {
   let html = "";
-  task.forEach(element => {
-    let checked="";
-    if (element.isDone){
+  tasks.forEach(element => {
+    let checked = "";
+    if (element.isDone) {
       checked = "checked";
     }
     html += "<li><input type='checkbox'" + checked + "/>" + element.isDone + "-" + element.name + "-" + element.responsible + "</li>"
