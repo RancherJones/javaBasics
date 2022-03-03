06e1ff09b25195254f4ec0ffc4f10cb6efc03560
Vincent Jones <vinc.alex.jones@gmail.com>
2022-03-03 11:19:27 +0100

    react styling

--- Node/reactdemo/src/App.js
+++ Node/reactdemo/src/App.js
@@ -1,5 +1,6 @@
 
 import './App.css';
+import ButtonRow from './components/buttonRow';
 import SuperButton from './components/SuperButton';
 
 
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
       <h1>Helo</h1>
       <SuperButton/>
+      <ButtonRow/>
     </div>
   );
 }
--- Node/reactdemo/src/components/SuperButton.js
+++ Node/reactdemo/src/components/SuperButton.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react"
+import styles from './SuperButton.module.css'
 
 export default class SuperButton extends Component{
   render(){
-    return (<div>
+    return (<div className={styles.heading}>
       <button>button</button>
     </div>)
   }
