8ca7c6e5195d01c7421c2ddb37da882f640580fe
Vincent Jones <vinc.alex.jones@gmail.com>
2022-03-09 10:21:25 +0100

    Komponente Fortgeschritten gestartet

---
+++ Node/komponente_fortgeschritten/src/Komponente/list.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+
+export default class List extends Component{
+  render(){
+    return(
+      <div class = "List__list">
+        <li>eins</li>
+        <li>eins</li>
+        <li>eins</li>
+      </div>
+    )
+  }
+}
