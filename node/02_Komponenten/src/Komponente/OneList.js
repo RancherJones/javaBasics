38041bed242d420bbd0a1edaa8bb794c15a97eed
Vincent Jones <vinc.alex.jones@gmail.com>
2022-03-17 10:31:21 +0100

    fertig mit aufgabe 5

--- node/02_Komponenten/src/Komponente/OneList.js
+++ node/02_Komponenten/src/Komponente/OneList.js
@@ -2,11 +2,14 @@ import React, { Component } from "react"
 import styles from './OneList.module.css'
 
 export default class OneList extends Component{
+  constructor(name){
+    this.name = name
+  }
   render(){
     return(
       <div className={styles.onelist}>
         
-        <li className={styles.entry}>eins</li>
+        <li className={styles.entry}>name</li>
       
       </div>
     )
--- node/02_Komponenten/src/Komponente/Shortlist.js
+++ node/02_Komponenten/src/Komponente/Shortlist.js
@@ -1,14 +1,13 @@
 import React, { Component } from "react"
-import OneList from "./OneList"
 import styles from './ShortList.module.css'
 
 export default class ShortList extends Component{
   render(){
     return(
       <div className={styles.shortlist}>
-        <OneList></OneList>
-        <OneList/>
-        <OneList/>
+        <li>Coffe</li>
+        <li>tea</li>
+        <li>hot</li>
       </div>
     )
   }
