538f365af0db85143c0332dd9473e7265393723c
Vincent Jones <vinc.alex.jones@gmail.com>
2022-03-30 10:13:21 +0200

    counter React

--- node/01_Basics/reactdemo/src/App.js
+++ node/01_Basics/reactdemo/src/App.js
@@ -1,10 +1,14 @@
 
+import { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
+import CounterFunc from './components/CounterFunc';
 import Vote from './components/Vote';
 
 
 
 function App() {
+  
   return (
     <div className="App">
       <h1>Helo</h1>
@@ -12,7 +16,14 @@ function App() {
         <Vote candidate="Sepp" address="Dornbirn"/>
         <Vote candidate="John" address="Hohenems"/>
         <Vote candidate="Ella" address="Altacj"/>
+
+        
       </div>
+      <div>functional components
+        <Counter/>
+        <CounterFunc/>
+      </div>
+      
     </div>
 
   );
